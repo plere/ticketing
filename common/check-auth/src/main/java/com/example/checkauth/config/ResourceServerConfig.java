6fc176508f935295ba5bb9294c12c5e27ee1033b
ansy <plere0011@gmail.com>
2025-09-14 21:50:27 +0900

    @Order 어노테이션 제거

--- common/check-auth/src/main/java/com/example/checkauth/config/ResourceServerConfig.java
+++ common/check-auth/src/main/java/com/example/checkauth/config/ResourceServerConfig.java
@@ -1,9 +1,7 @@
 package com.example.checkauth.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ResourceServerConfig {
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
