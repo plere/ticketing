50de8e3509120bab2f3a1d13dad5e1a5540a9d88
ansy <plere0011@gmail.com>
2025-09-14 15:38:11 +0900

    리소스서버에서 jwt 검증을 할 수 있도록 모듈 추가

---
+++ common/check-auth/src/main/java/com/example/checkauth/config/ResourceServerConfig.java
@@ -0,0 +1,31 @@
+package com.example.checkauth.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig {
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            ).oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(Customizer.withDefaults())
+            );
+
+        return http.build();
+    }
+}
---
+++ common/check-auth/src/test/java/com/example/checkauth/CheckAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
