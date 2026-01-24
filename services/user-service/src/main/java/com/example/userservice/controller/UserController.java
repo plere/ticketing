c95dbc4271932949f6259b3e00600bebf7da0cf6
ansy <plere0011@gmail.com>
2026-01-25 00:04:02 +0900

    [user] 브라우저 리다이렉팅을 위해 로그인 로직 수정(state를 통해 요청한 경로로 302응답)

--- services/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ services/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -3,13 +3,16 @@ package com.example.userservice.controller;
 import com.example.httpresponse.response.CreatedResponseDto;
 import com.example.httpresponse.response.ResponseDto;
 import com.example.userservice.controller.dto.SignUpRequest;
-import com.example.userservice.controller.dto.UserLoginResponseDto;
+import com.example.userservice.external.auth.dto.LoginResponseDto;
 import com.example.userservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.example.userservice.controller.dto.UserResponseCode.LOGIN_SUCCESS;
 import static com.example.userservice.controller.dto.UserResponseCode.SIGNUP_SUCCESS;
 
 @RestController
@@ -18,13 +21,40 @@ import static com.example.userservice.controller.dto.UserResponseCode.SIGNUP_SUC
 public class UserController {
     private final UserService userService;
 
+    @GetMapping(value = "/oauth2/login")
+    public ResponseEntity<Void> oauthLogin(@RequestParam String state) {
+        return ResponseEntity
+            .status(HttpStatus.FOUND)
+            .header(HttpHeaders.LOCATION, userService.oauthLogin(state))
+            .build();
+    }
+
     @GetMapping(value = "/login")
-    public ResponseDto<UserLoginResponseDto> login(@RequestParam String code) {
-        return ResponseDto.from(LOGIN_SUCCESS, userService.login(code));
+    public ResponseEntity<Void> login(@RequestParam String code, @RequestParam String state) {
+        LoginResponseDto loginInfo = userService.login(code);
+
+        return ResponseEntity
+            .status(HttpStatus.FOUND)
+            .header(HttpHeaders.LOCATION, state)
+            .header(HttpHeaders.SET_COOKIE, ResponseCookie.from("accessToken", loginInfo.access_token())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(60)
+                .domain("mock-ticketing.com")
+                .build().toString())
+            .header(HttpHeaders.SET_COOKIE, ResponseCookie.from("refreshToken", loginInfo.refresh_token())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(1800)
+                .domain("mock-ticketing.com")
+                .build().toString())
+            .build();
     }
 
     @PostMapping
-    public ResponseDto<CreatedResponseDto> signUp(@RequestBody @Valid SignUpRequest request) {
+    public ResponseEntity<ResponseDto<CreatedResponseDto>> signUp(@RequestBody @Valid SignUpRequest request) {
         return CreatedResponseDto.from(userService.create(request), SIGNUP_SUCCESS);
     }
 }
--- services/user-service/src/main/java/com/example/userservice/service/UserService.java
+++ services/user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -1,9 +1,9 @@
 package com.example.userservice.service;
 
 import com.example.userservice.controller.dto.SignUpRequest;
-import com.example.userservice.controller.dto.UserLoginResponseDto;
 import com.example.userservice.external.auth.AuthClient;
 import com.example.userservice.external.auth.config.ClientProperty;
+import com.example.userservice.external.auth.dto.LoginResponseDto;
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.service.validation.CreateUserValidation;
 import lombok.RequiredArgsConstructor;
@@ -28,15 +28,17 @@ public class UserService {
         return userRepository.save(request.toModel(passwordEncryptor)).getId();
     }
 
-    public UserLoginResponseDto login(String code) {
-        return UserLoginResponseDto.from(
-            authClient.login(
-                getBasicAuth(),
-                Map.of("grant_type", clientProperty.getGrant_type(),
-                    "code", code,
-                    "redirect_uri", clientProperty.getRedirect_uri()
-                ))
-        );
+    public String oauthLogin(String state) {
+        return " https://mock-ticketing.com:8084/oauth2/authorize?response_type=code&client_id=user-service&redirect_uri=https://mock-ticketing.com:8081/users/login&scope=profile&state=" + state;
+    }
+
+    public LoginResponseDto login(String code) {
+        return authClient.login(
+            getBasicAuth(),
+            Map.of("grant_type", clientProperty.getGrant_type(),
+                "code", code,
+                "redirect_uri", clientProperty.getRedirect_uri()
+            ));
     }
 
     private String getBasicAuth() {
