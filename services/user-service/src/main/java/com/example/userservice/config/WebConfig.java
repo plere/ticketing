324d1bbec7dc9a55886b5f550a0afd4234fd4d2c
ansy <plere0011@gmail.com>
2026-02-07 16:56:45 +0900

    [user] cors 설정 추가

---
+++ services/user-service/src/main/java/com/example/userservice/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+            .allowedHeaders("Accept", "Cache-Control", "Cookie", "Content-Type", "Origin", "Authorization")
+            .allowCredentials(true)
+            .allowedOriginPatterns("*");
+    }
+}
