2a845e50ab26238df1b807a868d4fc807b5dd42e
ansy <plere0011@gmail.com>
2026-05-27 17:28:29 +0900

    [waiting] 스웨거 추가

--- services/waiting-service/src/main/java/com/example/waitingservice/reservationtoken/adapter/in/web/api/ReservationTokenController.java
+++ services/waiting-service/src/main/java/com/example/waitingservice/reservationtoken/adapter/in/web/api/ReservationTokenController.java
@@ -5,6 +5,8 @@ import com.example.waitingservice.reservationtoken.adapter.in.web.response.Reser
 import com.example.waitingservice.reservationtoken.application.service.ReservationTokenService;
 import com.example.waitingservice.reservationtoken.model.ReservationToken;
 import com.example.waitingservice.waitingtoken.model.WaitingToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "reservation-token")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reservation-token")
@@ -20,6 +23,7 @@ public class ReservationTokenController {
     private final ReservationTokenService reservationTokenService;
 
     @GetMapping("/concerts/{id}/{token}")
+    @Operation(summary = "웨이팅 토큰으로 예매 토큰 얻는 API")
     public ResponseEntity<ResponseDto<ReservationToken>> get(@PathVariable String id, @PathVariable String token) {
         return ResponseDto.from(HttpStatus.CREATED.value(), ReservationTokenResponseCode.CREATED_RESERVATION_TOKEN,
             reservationTokenService.create(
@@ -32,6 +36,7 @@ public class ReservationTokenController {
     }
 
     @GetMapping("/concerts/{id}/valid/{token}")
+    @Operation(summary = "예매 토큰이 유효성 확인 API")
     //Todo internal로 이동
     public ResponseEntity<ResponseDto<Boolean>> isValid(@PathVariable String id, @PathVariable String token) {
         return ResponseDto.from(ReservationTokenResponseCode.CHECK_RESERVATION_TOKEN_VALIDATION,
--- services/waiting-service/src/main/java/com/example/waitingservice/waitingtoken/adapter/in/web/api/WaitingTokenController.java
+++ services/waiting-service/src/main/java/com/example/waitingservice/waitingtoken/adapter/in/web/api/WaitingTokenController.java
@@ -4,6 +4,8 @@ import com.example.httpresponse.response.ResponseDto;
 import com.example.waitingservice.waitingtoken.adapter.in.web.response.WaitingTokenResponseCode;
 import com.example.waitingservice.waitingtoken.application.service.WaitingTokenService;
 import com.example.waitingservice.waitingtoken.model.WaitingToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "waiting-token")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/waiting-token")
@@ -19,11 +22,13 @@ public class WaitingTokenController {
     private final WaitingTokenService waitingTokenService;
 
     @GetMapping("/concerts/{concertId}/rounds/{roundId}")
+    @Operation(summary = "특정 콘서트 회차에 대해 웨이팅 토큰 얻는 API")
     public ResponseEntity<ResponseDto<WaitingToken>> get(@PathVariable Long concertId, @PathVariable Long roundId) {
         return ResponseDto.from(HttpStatus.CREATED.value(), WaitingTokenResponseCode.CREATED_WAITING_TOKEN, waitingTokenService.create(concertId, roundId));
     }
 
     @GetMapping("/position/concerts/{id}/{token}")
+    @Operation(summary = "웨이팅 토큰의 현재 대기열 위치 조회 API")
     public ResponseEntity<ResponseDto<Integer>> get(@PathVariable String id, @PathVariable String token) {
         return ResponseDto.from(
             WaitingTokenResponseCode.GET_MY_WAITING_POSITION,
