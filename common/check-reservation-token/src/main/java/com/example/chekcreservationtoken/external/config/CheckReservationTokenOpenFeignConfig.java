06d1ed12ebcd02611d837d0a3102268dd787979a
ansy <plere0011@gmail.com>
2026-06-06 15:47:53 +0900

    [m) check-reservation-token] 빈 중복 방지를 위해 빈 이름 변경 및 feignclient 패키지 범위 변경

--- common/check-reservation-token/src/main/java/com/example/chekcreservationtoken/external/config/CheckReservationTokenOpenFeignConfig.java
+++ common/check-reservation-token/src/main/java/com/example/chekcreservationtoken/external/config/CheckReservationTokenOpenFeignConfig.java
@@ -3,6 +3,7 @@ package com.example.chekcreservationtoken.external.config;
 import feign.QueryMapEncoder;
 import feign.Retryer;
 import feign.querymap.FieldQueryMapEncoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +11,16 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.example")
-public class OpenFeignConfig {
+@EnableFeignClients(basePackages = "com.example.chekcreservationtoken")
+public class CheckReservationTokenOpenFeignConfig {
     @Bean
+    @ConditionalOnMissingBean
     public Retryer.Default retryer() {
         return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(2L), 2);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public QueryMapEncoder queryMapEncoder() {
         return new FieldQueryMapEncoder();
     }
