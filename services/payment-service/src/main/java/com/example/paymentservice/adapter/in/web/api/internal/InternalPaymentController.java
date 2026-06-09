3cc5cfdec28f4569834d1e8597801b5ece299d55
ansy <plere0011@gmail.com>
2026-06-09 14:11:53 +0900

    [payment] 스웨거 정보 추가

--- services/payment-service/src/main/java/com/example/paymentservice/adapter/in/web/api/internal/InternalPaymentController.java
+++ services/payment-service/src/main/java/com/example/paymentservice/adapter/in/web/api/internal/InternalPaymentController.java
@@ -6,6 +6,8 @@ import com.example.paymentservice.adapter.in.web.request.ExecutePaymentRequest;
 import com.example.paymentservice.adapter.in.web.request.ReadyPaymentRequest;
 import com.example.paymentservice.port.in.ExecutePaymentUseCase;
 import com.example.paymentservice.port.in.ReadyPaymentUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.example.paymentservice.adapter.in.web.response.PaymentResponseCode.READY_PAYMENT_SUCCESS;
 
+@Tag(name = "Payment/internal")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/internal/payment")
@@ -23,13 +26,13 @@ public class InternalPaymentController {
     private final ExecutePaymentUseCase executePaymentUseCase;
 
     @PostMapping
-    public ResponseEntity<ResponseDto<CreatedResponseDto>> execute(@RequestBody ExecutePaymentRequest request) {
+    @Operation(summary = "결제 진행 API")
     public ResponseEntity<ResponseDto<CreatedResponseDto<Void>>> execute(@RequestBody ExecutePaymentRequest request) {
         return CreatedResponseDto.from(executePaymentUseCase.execute(request.toCommand()).id(), READY_PAYMENT_SUCCESS);
     }
 
     @PostMapping("/ready")
-    public ResponseEntity<ResponseDto<CreatedResponseDto>> ready(@RequestBody ReadyPaymentRequest request) {
+    @Operation(summary = "결제 전 준비 API")
     public ResponseEntity<ResponseDto<CreatedResponseDto<Void>>> ready(@RequestBody ReadyPaymentRequest request) {
         return CreatedResponseDto.from(readyPaymentUseCase.ready(request.toCommand()).id(), READY_PAYMENT_SUCCESS);
     }
