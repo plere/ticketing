cac3338cbee49350c25804831e263371975b1f28
ansy <plere0011@gmail.com>
2026-06-11 16:34:28 +0900

    [reservation] 외부호출 timeout 시간 변경

--- services/reservation-service/src/main/java/com/example/reservationservice/common/config/OpenFeignConfig.java
+++ services/reservation-service/src/main/java/com/example/reservationservice/common/config/OpenFeignConfig.java
@@ -5,13 +5,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 @Configuration
 @EnableFeignClients(basePackages = "com.example.reservationservice")
 public class OpenFeignConfig {
     @Bean
     public Retryer.Default retryer() {
-        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(2L), 2);
+        return new Retryer.Default(100L, 5000, 2);
     }
 }
